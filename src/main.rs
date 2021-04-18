932e0949ccafa35f1d635a5ae157960474496249
Faheetah <Faheetah@users.noreply.github.com>
2021-04-17 23:14:02 -0500

    Use inherited stdin pipe

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::env;
 use std::ffi::OsStr;
-use std::io::Write;
-use std::io::{self, Read};
+use std::io::Read;
 use std::path::Path;
 use std::process::{Command, Stdio};
 use std::str;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
 #[derive(Serialize, Deserialize)]
 struct Output {
@@ -25,45 +23,25 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let command = Path::new(&args[0]).file_name().unwrap();
 
-    let mut buffer = vec![];
-    let mut stdin = io::stdin();
-    stdin.read_to_end(&mut buffer).unwrap();
-    let stdin_str = String::from_utf8(buffer).unwrap();
-
-    let v: Value;
-    let stdin_data = if stdin_str == "" {
-        ""
-    } else {
-        v = serde_json::from_str(&stdin_str).unwrap();
-        &v["stdin"].as_str().unwrap()
-    };
-
     if command == OsStr::new("bee") {
         let (arg_0, cmd_args) = args.split_at(2);
-        run_command(&arg_0[1], cmd_args, stdin_data)
+        run_command(&arg_0[1], cmd_args)
     } else {
         let (arg_0, cmd_args) = args.split_at(1);
         let command = Path::new(&arg_0[0]).file_name().unwrap();
         let s = command.to_str().unwrap().to_owned();
-        run_command(&s, cmd_args, stdin_data)
+        run_command(&s, cmd_args)
     }
 }
 
-fn run_command(cmd: &String, args: &[String], stdin: &str) {
+fn run_command(cmd: &String, args: &[String]) {
     let mut process = Command::new(cmd)
         .args(args)
-        .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
         .unwrap();
 
-    process
-        .stdin
-        .take()
-        .unwrap()
-        .write_all(stdin.as_bytes())
-        .unwrap();
     let mut out = process.stdout.take().unwrap();
     let mut err = process.stderr.take().unwrap();
 
